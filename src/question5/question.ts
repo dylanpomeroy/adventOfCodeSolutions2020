33df311162316dd5f41dee7ad5b7923b47599a78
Dylan Pomeroy <dylan@dylanpomeroy.com>
2020-12-05 01:39:50 -0500

    Optimize part A of question 5

--- src/question5/question.ts
+++ src/question5/question.ts
@@ -1,29 +1,11 @@
-const getSeatId = (boardingPass: string, rowCount: number = 127, columnCount: number = 8) => {
-  let leftIndex = 0
-  let rightIndex = 127
-  let boardingPassIndex = 0
-  while (boardingPassIndex < 7) {
-    if (boardingPass[boardingPassIndex] == 'F')
-      rightIndex = Math.floor((rightIndex + leftIndex) / 2)
-    else
-      leftIndex = Math.ceil((rightIndex + leftIndex) / 2)
-    boardingPassIndex++
-  }
+const getSeatId = (boardingPass: string) => {
+  const binary = boardingPass
+    .replace(/F/g, '0')
+    .replace(/B/g, '1')
+    .replace(/L/g, '0')
+    .replace(/R/g, '1')
 
-  const row = leftIndex
-  leftIndex = 0
-  rightIndex = 7
-  while (boardingPassIndex < 10) {
-    if (boardingPass[boardingPassIndex] == 'L')
-      rightIndex = Math.floor((rightIndex + leftIndex) / 2)
-    else
-      leftIndex = Math.ceil((rightIndex + leftIndex) / 2)
-    boardingPassIndex++
-  }
-
-  const column = leftIndex
-
-  return row * 8 + column
+  return parseInt(binary, 2)
 }
 
 const partA = async (input: string[]) => {
